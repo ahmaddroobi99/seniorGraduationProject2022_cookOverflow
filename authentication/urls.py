effa6461e7d09d1119f6b921107e241e1a049044
ahmaddroobi99 <ahmad.droobi1999@gmail.com>
2022-01-26 16:50:44 +0200

    Landing PAges in ,up out Done

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
    path('',views.home ,name= "home"),
-   path('signup', views.home, name="signup"),
-   path('signin', views.home, name="signin"),
-   path('signout', views.home, name="signout"),
+   path('signup', views.signup, name="signup"),
+   path('signin', views.signin, name="signin"),
+   path('signout', views.signout, name="signout"),
 
 ]
