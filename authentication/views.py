4a303bc18fd0a476b804a8bfc6a416e1a6e49c8b
ahmaddroobi99 <ahmad.droobi1999@gmail.com>
2022-01-26 13:49:28 +0200

    Landing PAges in ,up out Done

--- authentication/views.py
+++ authentication/views.py
@@ -5,4 +5,14 @@ from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("hello I am Working ")
+    # return HttpResponse("hello I am Working ")
+    return render(request, "authentication/index.html")
+
+def signup (request) :
+    return render(request,"authentication/signup.html")
+
+def signin (request) :
+    return render(request,"authentication/signin.html")
+
+def signout (request) :
+    return render(request,"authentication/signout.html")
