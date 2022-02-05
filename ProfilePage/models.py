89ef93a8bb2ba6429b1bb4b0f2a128a817f3ec4f
Ataa Shaqour <ataa.shaqour.as@gmail.com>
2022-02-05 19:04:31 +0200

    Change Users to Cookers and make an inheritance between Cooker and User auth + install restframework to make Crud fanctionality + Profile module V2

--- ProfilePage/models.py
+++ ProfilePage/models.py
@@ -1,20 +1,71 @@
 from distutils.command.upload import upload
-from tkinter import CASCADE
+from enum import unique
+from django.utils import timezone
+from unicodedata import decimal
 import django
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.forms import EmailField
 
 # Create your models here.
 
-class Users(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class CustomAccountManager(BaseUserManager):
+
+    def create_superuser(self, email, user_name, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_active', True)
+        other_fields.setdefault('is_superuser', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must be assigned to is_staff=True")
+        
+        if other_fields.get('is_active') is not True:
+            raise ValueError("Superuser must be assigned to is_active=True")
+
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must be assigned to is_superuser=True")
+
+        return self.create_user(email, user_name, password, **other_fields)
+
+    def create_user(self, email, user_name, password, **other_fields):
+        
+        if not email:
+            raise ValueError("You must provide an email address.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+class Cooker(AbstractBaseUser, PermissionsMixin):
+
+    user_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    dob = models.DateField(null=True)
+    start_date = models.DateField(default=timezone.now)
     profile_photo = models.ImageField(upload_to=('../static/ProfileImgs'))
     background_photo = models.ImageField(upload_to=('../static/ProfileImgs'))
+    bio = models.TextField(null=True)
     phoneNumber = models.IntegerField(null=True)
     country = models.CharField(max_length=50)
     job = models.CharField(max_length=50)
-    dob = models.DateField()
+    rating = models.FloatField(null=False, default=0)
     followers = models.ManyToManyField('self', through='Follower', symmetrical=False, related_name='related_to')
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS  = ['user_name']
+
+    def __str__(self):
+        return self.user_name
+
+    # create / Insert / add - POST
+    # Retrieve / Fetch - GET
+    # Update / Edit - PUT
+    # Delete / Remove - DELETE
 
     def follow(self, user, status):
         follower, created = Follower.objects.get_or_create(
@@ -55,17 +106,17 @@ relationship_status = (
     )
 
 class Follower(models.Model):
-    fromUser = models.ForeignKey(Users, related_name='from_user', on_delete=models.CASCADE)
-    toUser = models.ForeignKey(Users, related_name='to_user', on_delete=models.CASCADE)
+    fromUser = models.ForeignKey(Cooker, related_name='from_user', on_delete=models.CASCADE)
+    toUser = models.ForeignKey(Cooker, related_name='to_user', on_delete=models.CASCADE)
     status = models.IntegerField(choices=relationship_status)
 
 class Post(models.Model):
     postType = models.CharField(max_length=50, null=False)
     postDescription = models.TextField(null=False)
-    userID = models.ForeignKey(Users, on_delete=models.CASCADE)
+    userID = models.ForeignKey(Cooker, on_delete=models.CASCADE)
     
 class Share(models.Model):
     shareName = models.CharField(max_length=50, null=False)
     shareType = models.CharField(max_length=50, null=False)
     shareDescription = models.TextField(null=False)
-    userID = models.ForeignKey(Users, on_delete=models.CASCADE)
+    userID = models.ForeignKey(Cooker, on_delete=models.CASCADE)
--- cookOverflow/settings.py
+++ cookOverflow/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authentication',
+    'rest_framework',
     'ProfilePage',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'ProfilePage.Cooker' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
