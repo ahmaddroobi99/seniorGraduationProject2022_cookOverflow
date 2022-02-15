205939d7e03fbe5c44db5619c8689c8c3d28ba7f
ahmaddroobi99 <ahmad.droobi1999@gmail.com>
2022-02-15 19:26:00 +0200

    Profile Module+ User Done

--- cookOverflow/info.py
+++ cookOverflow/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ahmad.droobi1999@gmail.com'
-EMAIL_HOST_PASSWORD = '404729782Ahmad*'
+EMAIL_HOST_USER = 'cookoverflow2022@gmail.com'
+EMAIL_HOST_PASSWORD = '0592222890'
 EMAIL_PORT = 587
