975b1ff79e1e2bc34a3c1f4f88498ac0285ce750
Ataa00 <ataa.shaqour.as@gmail.com>
2022-04-10 20:05:45 -0700

    post view

--- Timeline/urls.py
+++ Timeline/urls.py
@@ -5,5 +5,6 @@ app_name = "Timeline"
 
 urlpatterns = [
     path('post/create', PostCreateView.as_view(), name="post-create"),
+    path('post/edit/<int:pk>', update_post, name="post-edit"),
     path('comment/create/<int:post_id>', create_comment, name="comment-create"),
 ]
